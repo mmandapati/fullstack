919a706002e681dcbb4e9191f5ea4ade401ed2f4
mmandapati <monicalakshmi.mandapati@sjsu.edu>
2023-03-14 22:00:17 -0700

    First-Change

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">EasyBuy</a>
+      </header>
+      <main>Product Lists</main>
+    </div>
+  );
+}
+
+export default App;
